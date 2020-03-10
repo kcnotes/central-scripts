c64f630f00f0afa2b6da26baf9ee6f05e2b01d62
Kris <kris.choy1@gmail.com>
2020-03-10 22:42:29 +1100

    Small modifications

--- VerifyUser.js
+++ VerifyUser.js
@@ -5,10 +5,12 @@
  * 
  * @author Noreplyz
  */
-;(function(window, $, mw, Mustache) {
+;
+(function (window, $, mw, Mustache) {
     if (!mw.config.get('wgPageName').match(/^Special:VerifyUser.*/)) return;
 
-    var templates = {}, verifyUser = {};
+    var templates = {},
+        verifyUser = {};
 
     /**
      * Trims URL and other fluff from a username
@@ -31,40 +33,40 @@
     }
 
     // Main template shown to all users
-    templates.main = 
+    templates.main =
         '<div style="text-align:center;line-height:180%;font-family:\'Rubik\';">' +
-        '{{#username}}' + 
-            '{{^usernameCompare}}' +
-                '<div style="color:#ee1a41;font-weight:bold">' +
-                    '{{#i18n}}error-loggedin{{/i18n}}' +
-                '</div>' + 
-            '{{/usernameCompare}}' +
-            '{{#i18n}}verify-instructions{{/i18n}}<br/><br/>' + 
-            '<input placeholder="discord#0000" value="{{discordHandle}}" style="padding:8px; width:350px;font-family:\'Rubik\';font-size:20px" id="verify-input"/> ' +
-            '<div class="wds-button" type="submit" style="vertical-align:middle;cursor:pointer;" id="verify"><span>{{#i18n}}button-verify{{/i18n}}</span></div>' +
-            '<br/><br/><small>{{#i18n}}verify-notice{{/i18n}}</small>' +
-        '{{/username}}' + 
-        '{{^username}}' + 
-            '{{#i18n}}verify-login{{/i18n}} <a href="https://help.gamepedia.com/Discord" class="external">{{#i18n}}verify-gamepedia{{/i18n}}</a><br/><br/>' + 
-            '<a href="https://www.fandom.com/signin?redirect={{backlink}}" style="text-decoration:none">' +
-                '<div class="wds-button" style="cursor:pointer;"><span>{{#i18n}}button-login{{/i18n}}</span></div>' + 
-            '</a>' +
+        '{{#username}}' +
+        '{{^usernameCompare}}' +
+        '<div style="color:#ee1a41;font-weight:bold">' +
+        '{{#i18n}}error-loggedin{{/i18n}}' +
+        '</div>' +
+        '{{/usernameCompare}}' +
+        '{{#i18n}}verify-instructions{{/i18n}}<br/><br/>' +
+        '<input placeholder="discord#0000" value="{{discordHandle}}" style="padding:8px; width:350px;font-family:\'Rubik\';font-size:20px" id="verify-input"/> ' +
+        '<div class="wds-button" type="submit" style="vertical-align:bottom;cursor:pointer;line-height:inherit;" id="verify"><span>{{#i18n}}button-verify{{/i18n}}</span></div>' +
+        '<br/><br/><small>{{#i18n}}verify-notice{{/i18n}}</small>' +
+        '{{/username}}' +
+        '{{^username}}' +
+        '{{#i18n}}verify-login{{/i18n}} <a href="https://help.gamepedia.com/Discord" class="external">{{#i18n}}verify-gamepedia{{/i18n}}</a><br/><br/>' +
+        '<a href="https://www.fandom.com/signin?redirect={{backlink}}" style="text-decoration:none">' +
+        '<div class="wds-button" style="cursor:pointer;"><span>{{#i18n}}button-login{{/i18n}}</span></div>' +
+        '</a>' +
         '{{/username}}' +
         '</div>';
-    
+
     // Template shown after a Discord handle is submitted
     templates.complete =
         '<div style="text-align:center;line-height:180%;font-family:\'Rubik\';">' +
         '{{#i18n}}verify-complete{{/i18n}}<br/><br/>' +
         '<input value="!verify {{username}}" onClick="this.select();" style="padding:8px; width:350px;font-family:\'Rubik\';font-size:20px" readonly/> ' +
         '</div>';
-    
+
     templates.error =
         '<div style="color:#ee1a41;font-weight:bold">' +
-            '{{#i18n}}general-error{{/i18n}} <br/>' +
-            '{{error}}' +
+        '{{#i18n}}general-error{{/i18n}} <br/>' +
+        '{{error}}' +
         '</div>';
-    
+
     verifyUser.servicesHost = 'https://services.fandom.com/';
 
     verifyUser._setDiscordHandle = function (userid, discordHandle) {
@@ -80,15 +82,15 @@
         });
     }
 
-    verifyUser.toi18n = function() {
-        return function(text, render) {
+    verifyUser.toi18n = function () {
+        return function (text, render) {
             return render(mw.html.escape(verifyUser.i18n.msg(text).plain()));
         }
     };
-    
+
     // Starts the script
     verifyUser.init = function (i18n) {
-        i18n.loadMessages('VerifyUser').done(function(i18n) {
+        i18n.loadMessages('VerifyUser').done(function (i18n) {
             verifyUser.i18n = i18n;
 
             // Update header/title
@@ -99,7 +101,7 @@
                 discordHandle = '',
                 providedUsername = pagename.indexOf('/') > -1 ? pagename.replace(/^.*\//g, '') : '',
                 userid = mw.config.get('wgUserId');
-            
+
             if (mw.util.getParamValue('user') && mw.util.getParamValue('tag')) {
                 discordHandle = mw.util.getParamValue('user') + '#' + mw.util.getParamValue('tag');
             }
@@ -112,22 +114,22 @@
                 discordHandle: discordHandle,
                 i18n: verifyUser.toi18n
             }));
-    
-            $('#verify').on('click', function() {
-                $('#WikiaArticle').empty().append(i18n.msg('loading').plain());
+
+            $('#verify').on('click', function () {
                 verifyUser._setDiscordHandle(userid, $('#verify-input').val()).done(function (data) {
                     $('#WikiaArticle').empty().append(Mustache.render(templates.complete, {
                         username: username,
                         i18n: verifyUser.toi18n
                     }));
-                }).fail(function(e) {
+                }).fail(function (e) {
                     $('#WikiaArticle').empty().append(Mustache.render(templates.error, {
                         error: JSON.parse(e.responseText).title,
                         i18n: verifyUser.toi18n
                     }));
                 });
+                $('#WikiaArticle').empty().append(i18n.msg('loading').plain());
             });
-    
+
             $('#verify-input').keypress(function (e) {
                 if (e.which === 13) {
                     $('#verify').click();
@@ -135,11 +137,12 @@
             });
         })
     };
-    
+
     importArticle({
         type: 'script',
         article: 'u:dev:MediaWiki:I18n-js/code.js'
     });
     mw.hook('dev.i18n').add(verifyUser.init);
- 
+
 })(window, jQuery, mediaWiki, Mustache);
+    
